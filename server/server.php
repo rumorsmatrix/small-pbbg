b297a0124d3b24d7bf35860b6590e0cfb1049ecd
rumorsmatrix <threesides@gmail.com>
2020-03-31 14:46:45 +0000

    server/server.php

---
+++ server/server.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Rumorsmatrix\Mud\Server as Server;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+// eloquent setup
+$capsule = new Capsule;
+$db_config = include(__DIR__ . '/config/db.php');
+$capsule->addConnection($db_config);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+// websocket server setup
+$server = new Server('wss://philcooper.org:8080', __DIR__ . '/rumorsmatrix.pem');
+$server->startListening();
+
+
